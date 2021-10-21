0b09802dea5507e58c144829e67a9d18ca73f2ee
Lucas de Farias Teixeira <programmer.lucas.teixeira@gmail.com>
2021-10-21 10:05:02 -0300

    Configurações locais

---
+++ CRUD/CRUD/local_settings.py
@@ -0,0 +1,16 @@
+#Chave secreta do Django e configurações do banco de dados
+
+SECRET_KEY = '#%tfelphv2nt6ccn5pf10mmub1dv+i-tb$w*u4wrl*iup$&pbd'
+
+DB = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'bvahbsztztgg6sikbd5r',
+        'USER': 'uqiscdvrw78it6dr',
+        'PASSWORD': '3f1AR6qvGnzSiADaXztY',
+        'HOST': 'bvahbsztztgg6sikbd5r-mysql.services.clever-cloud.com',
+        'PORT': '3306',
+    }
+}
+
+DATABASES = DB
